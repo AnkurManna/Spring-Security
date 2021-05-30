585cb26d3951d77ca74f78d5f9ed7f6f2af33dbe
Ankur Manna <ankurcasino108@gmail.com>
2021-05-30 19:58:05 +0530

    hardcoded datastore

--- src/main/java/bookapi/security/securityConfig.java
+++ src/main/java/bookapi/security/securityConfig.java
@@ -1,34 +1,33 @@
 package bookapi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-/* tells it is a configurtaion*/
+/* tells it is a configurtaion */
 @EnableWebSecurity
 public class securityConfig extends WebSecurityConfigurerAdapter{
 
+		@Autowired
+		UserDetailsService userDetailsService;
 		@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception
 		{
-			auth.inMemoryAuthentication()
-			.withUser("blah")
-			.password("blah")
-			.roles("USER")
-			.and()
-			.withUser("foo")
-			.password("foo")
-			.roles("ADMIN");
+			auth.userDetailsService(userDetailsService);
 		}
 		
+		@SuppressWarnings("deprecation")
 		@Bean
 		public PasswordEncoder getPasswordEncoder()
 		{
-			return new BCryptPasswordEncoder();
+			return NoOpPasswordEncoder.getInstance();
 		}
 		
 		@Override
