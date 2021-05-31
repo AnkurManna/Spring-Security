165190cf3d07a4e143996d5ffba29755665fc853
Ankur Manna <ankurcasino108@gmail.com>
2021-05-31 16:21:52 +0530

    with jwt

--- src/main/java/bookapi/controllers/BookController.java
+++ src/main/java/bookapi/controllers/BookController.java
@@ -1,13 +1,27 @@
 package bookapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import bookapi.models.AuthenticationRequest;
+import bookapi.models.AuthenticationResponse;
+import bookapi.services.MyUserDetailsService;
+import bookapi.utils.JwtUtils;
 
 
 @RestController
 public class BookController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public  String welcome()
 	{
 		//logger.trace("In hello world");
@@ -16,21 +30,35 @@ public class BookController {
 		return "Hello World";
 	}
 	
-	@GetMapping("/xx")
-	public String hello1()
-	{
-		return "<h1>Hello</h1>";
-	}
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtils jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
 	
-	@GetMapping("/admin")
-	public String admin()
-	{
-		return "Admin";
-	}
-	
-	@GetMapping("/user")
-	public String user()
-	{
-		return "User";
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 	}
 }
--- src/main/java/bookapi/security/securityConfig.java
+++ src/main/java/bookapi/security/securityConfig.java
@@ -2,19 +2,26 @@ package bookapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import bookapi.filters.JwtRequestFilter;
 
 /* tells it is a configurtaion */
 @EnableWebSecurity
 public class securityConfig extends WebSecurityConfigurerAdapter{
 
+		@Autowired
+		private JwtRequestFilter jwtRequestFilter;
 		@Autowired
 		UserDetailsService userDetailsService;
 		@Override
@@ -38,13 +45,23 @@ public class securityConfig extends WebSecurityConfigurerAdapter{
 			 * permitAll() is to allow to everything
 			 * more restrict route is placed in top
 			 * */
-			http.authorizeRequests()
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasRole("USER")
-			.antMatchers("/").permitAll()
+			http.csrf().disable()
+			.authorizeRequests().antMatchers("/authenticate")
+			.permitAll().
+			 anyRequest().authenticated()
+			.and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			;
 			
-			.and()
-			.formLogin();
+			http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		}
+		
+		@Override
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception{
+			
+			return super.authenticationManagerBean();
+		}
+		
 		
 }
